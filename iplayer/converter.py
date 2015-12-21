35aaa2ef1abbaa1e03cf90c0103aa771312e58e2
Petr Sedlacek <petr@sedlacek.biz>
2015-12-21 12:37:15 +0100

    Add converter.get_pid_info() function

--- iplayer/converter.py
+++ iplayer/converter.py
@@ -1,7 +1,9 @@
 import lxml.html
+import urllib2
 
 URL_BASE = 'http://www.iplayerconverter.co.uk'
 CONVERT = "/convert.aspx?pid={0:s}"
+INFO = "/pid/{0:s}/default.aspx"
 
 
 def pid2rtmpdump(pid):
@@ -22,3 +24,22 @@ def pid2rtmpdump(pid):
         print u"pid2rtmpdump: more than one <p><code> elements found: {0:d}" \
             .format(len(codes))
         return False
+
+
+def get_pid_info(pid):
+    """
+    Return pid info: vpid, aac link, hls playlist, wma link
+    :param pid: string
+    :rtype: dict
+    """
+    url = URL_BASE + INFO.format(pid)
+    fp = urllib2.urlopen(url)
+    info = {}
+    for line in fp:
+        if line[:4] == 'pid=':
+            for el in line.split(','):
+                ell = el.split('=')
+                if len(ell) == 2:
+                    info[ell[0]] = ell[1]
+            break
+    return info
