580c5d23ee6745e425e50c81442739e123f5a459
Petr Sedlacek <petr@sedlacek.biz>
2015-12-21 17:04:11 +0100

    Add downloader.download_hls() for downloading HLS streams

--- iplayer/downloader.py
+++ iplayer/downloader.py
@@ -1,6 +1,9 @@
 import converter
+import cookielib
 import os
 import subprocess
+import sys
+import urllib2
 
 
 def download_rtmp(pid, directory=''):
@@ -33,6 +36,80 @@ def download_rtmp(pid, directory=''):
     if retcode > 0:
         os.remove(file_path)
         return False
+    return file_path
+
+
+def download_hls(pid, directory='', progress=False):
+    """
+    Download an iPlayer episode from a HLS stream to a specified directory
+    :param pid: string
+    :param directory: string
+    :param progress: boolean show a download progress
+    :rtype: string|false output file path
+    """
+    playlist_url = converter.get_pid_info(pid).get('hls', False)
+    if not playlist_url:
+        return False
+
+    # create the directory if needed
+    if directory != '' and not os.path.isdir(directory):
+        try:
+            os.makedirs(directory)
+        except OSError as ex:
+            print u"Could not create directory {0:s}: {1:s}".format(directory, ex)
+            return False
+
+    # open the output file
+    filename = pid + '.ts'
+    file_path = os.path.join(directory, filename)
+    try:
+        ts_fp = open(file_path, 'w')
+    except IOError as ex:
+        print u"Could not create output file {0:s}: {1:s}".format(file_path, ex)
+        return False
+
+    # get the m3u playlist URL
+    cj = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    res = opener.open(playlist_url)
+    m3u_url = False
+    for line in res:
+        # skip comments
+        if line.lstrip()[:1] == '#':
+            continue
+        # first non-comment line is the m3u URL
+        m3u_url = line.strip()
+        break
+    if not m3u_url:
+        return False
+
+    # get the segment URLs from the M3U playlist using the same opener to send the required cookies
+    res = opener.open(m3u_url)
+    segments = []
+    for line in res:
+        if line.lstrip()[:1] == '#':
+            continue
+        segments.append(line.strip())
+
+    # download the segments and write them into a single file
+    step = 10
+    cur_percent = -step
+    cur_seg = 1
+    num_seg = len(segments)
+    for seg in segments:
+        res = opener.open(seg)
+        ts_fp.write(res.read())
+        if 100.0 * cur_seg / num_seg >= cur_percent + step:
+            cur_percent += step
+            if progress:
+                print "{0:d}%".format(cur_percent),
+                sys.stdout.flush()
+        cur_seg += 1
+    if progress:
+        if cur_percent < 100:
+            print '100%'
+        else:
+            print ''
     return file_path
 
 
